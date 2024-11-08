e640f2a6e4d4cea42b906da3f29d7a562926ec36
CJura <korolevpavel02@gmail.com>
2024-11-08 17:10:28 +0300

    toggle test btn

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import Button from "../Button/Button";
-import { useTelegram } from "../../hooks/useTelegram";
+import {useTelegram} from "../../hooks/useTelegram";
 
 const Header = () => {
-  const { user, onClose } = useTelegram();
+    const {user, onClose} = useTelegram();
 
-  return (
-    <div className={"header"}>
-      <Button onclick={onClose}>Закрыть</Button>
-      <span className={"username"}>{user?.first_name}</span>
-    </div>
-  );
+    return (
+        <div className={"header"}>
+            <Button onclick={onClose}>Закрыть</Button>
+            <span className={"username"}>
+                {user?.first_name}</span>
+        </div>
+    );
 };
 
 export default Header;
