feda4d7a5f3b38521b5dfb772cdcf6a05508afed
CJura <korolevpavel02@gmail.com>
2024-11-08 20:01:59 +0300

    toggle test btn

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 import { useTelegram } from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
 import {Route, Routes} from "react-router-dom";
-import ProductList from "./components/ProductList/ProductList";
 import Form from "./components/Form/Form";
+import ProductList from "./components/ProductList/ProductList";
 
 function App() {
   const { onToggleButton, tg } = useTelegram();
@@ -17,7 +17,7 @@ function App() {
     <div className="App">
       <Header />
     <Routes>
-        <Route index element={<ProductList />} />
+        <Route index element={<ProductList/>} />
         <Route path={"form"} element={<Form/>} />
     </Routes>
     </div>
